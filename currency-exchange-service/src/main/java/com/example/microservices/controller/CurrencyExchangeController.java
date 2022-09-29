052d63448d800bc12b1bbc685d13bfbbc9e7b249
ank-pandey <ankitkp7@gmail.com>
2022-09-29 23:23:31 +0530

    zipkin integration

--- currency-exchange-service/src/main/java/com/example/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.example.microservices.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import com.example.microservices.repo.CurrencyExchangeRepository;
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
 	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Environment env;
 	
@@ -23,6 +27,9 @@ public class CurrencyExchangeController {
 	@GetMapping("/from/{fromCurrency}/to/{toCurrency}")
 	public CurrencyExchange retrieveCurrencyExchange(@PathVariable String fromCurrency, 
 			@PathVariable String toCurrency){
+		
+		log.info("retrieveCurrencyExchange called with {} to {}", fromCurrency, toCurrency);
+		
 		 CurrencyExchange exchange = exchangeRepository.findByFromCurrencyAndToCurrency(fromCurrency, toCurrency);
 		 if(null == exchange) {
 			 throw new RuntimeException("There is no currency exchange data present from  "
